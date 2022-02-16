b154f7a8bb38197fae21122e010052f970935541
Jason Ngan <jason.ngan@shopee.com>
2022-02-16 13:30:51 +0800

    add drag handler

--- frontend/src/components/ViewContainer/DailyView/Task/Task.js
+++ frontend/src/components/ViewContainer/DailyView/Task/Task.js
@@ -1,10 +1,30 @@
-import { Paper } from "@mui/material";
 import React from "react"
 import "./Task.modules.scss"
 
+import { Checkbox, Paper, TextField } from "@mui/material";
+import DragHandleIcon from "@mui/icons-material/DragHandle";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 export default function Task() {
     return (
-            <Paper className="task-paper" elevation={4}>
-            </Paper>
+      <Paper className="task-paper" elevation={4}>
+        <div className="task-container">
+          <Checkbox
+            className={"checkbox-grid"}
+            checked={false}
+            disableRipple={true}
+          />
+          <TextField
+            className={"text-grid"}
+            variant="standard"
+            multiline={true}
+            InputProps={{ disableUnderline: true }}
+          />
+          <MoreVertIcon className={"icon-grid"} />
+        </div>
+        <div className="drag-container">
+          <DragHandleIcon/>
+        </div>
+      </Paper>
     );
 }
