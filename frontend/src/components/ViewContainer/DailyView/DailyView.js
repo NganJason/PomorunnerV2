2d20cc5349f960cd70c67ec55ca7dc54e22bcb10
Jason Ngan <jason.ngan@shopee.com>
2022-02-17 21:51:39 +0800

    add arrow button

--- frontend/src/components/ViewContainer/DailyView/DailyView.js
+++ frontend/src/components/ViewContainer/DailyView/DailyView.js
@@ -1,13 +1,18 @@
 import React from "react"
+import "./DailyView.modules.scss";
+
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import Tasklist from "./Tasklist/Tasklist";
-import "./DailyView.modules.scss"
 
 export default function DailyView() {
     return (
       <div className="dailyview-container">
+        <div className="arrow-container"><ArrowLeftIcon/></div>
         <div className="tasklist-container">
-          <Tasklist title={"15 Feb"}/>
+          <Tasklist title={"15 Feb"} />
         </div>
+        <div className="arrow-container"><ArrowRightIcon/></div>
       </div>
     );
 }
