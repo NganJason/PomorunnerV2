aaf98aae2ebcb59e872c48b756070401398a128f
Jason Ngan <jason.ngan@shopee.com>
2022-03-11 22:28:06 +0800

    add droppable

--- frontend/src/components/ViewContainer/DailyView/Task/Task.js
+++ frontend/src/components/ViewContainer/DailyView/Task/Task.js
@@ -4,6 +4,7 @@ import "./Task.modules.scss"
 import { Checkbox, TextField } from "@mui/material";
 import DragHandleIcon from "@mui/icons-material/DragHandle";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {Draggable} from "react-beautiful-dnd"
 
 export default function Task({
   idx,
@@ -12,31 +13,41 @@ export default function Task({
   toggleTaskIsDone,
 }) {
     return (
-      <div className="task-paper">
-        <div className="task-container">
-          <Checkbox
-            className={"checkbox-grid"}
-            checked={task.isDone}
-            disableRipple={true}
-            onChange={() => {
-              toggleTaskIsDone(idx);
-            }}
-          />
-          <TextField
-            className={"text-grid"}
-            variant="standard"
-            multiline={true}
-            InputProps={{ disableUnderline: true }}
-            value={task.content}
-            onChange={(e) => {
-              editTaskContent(idx, e.target.value);
-            }}
-          />
-          <MoreVertIcon className={"icon-grid"} />
-        </div>
-        <div className="drag-container">
-          <DragHandleIcon />
-        </div>
-      </div>
+      <Draggable draggableId={`${task.id}`} index={idx}>
+        {(provided) => {
+          return (
+            <div
+              className="task-paper"
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+            >
+              <div className="task-container">
+                <Checkbox
+                  className={"checkbox-grid"}
+                  checked={task.isDone}
+                  disableRipple={true}
+                  onChange={() => {
+                    toggleTaskIsDone(idx);
+                  }}
+                />
+                <TextField
+                  className={"text-grid"}
+                  variant="standard"
+                  multiline={true}
+                  InputProps={{ disableUnderline: true }}
+                  value={task.content}
+                  onChange={(e) => {
+                    editTaskContent(idx, e.target.value);
+                  }}
+                />
+                <MoreVertIcon className={"icon-grid"} />
+              </div>
+              <div className="drag-container" {...provided.dragHandleProps}>
+                <DragHandleIcon />
+              </div>
+            </div>
+          );
+        }}
+      </Draggable>
     );
 }
